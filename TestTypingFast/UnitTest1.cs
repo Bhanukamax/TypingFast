a67ee1528024cb70b36429d5515893fce189af86
Bhanuka <bhanukamax@gmail.com>
2024-12-27 23:25:25 +0530

    Loading words file

---
+++ TestTypingFast/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using TypingFast.Services;
+
+namespace TestTypingFast;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var wordsService = new WordsService();
+        var result = wordsService.GetWords(2);
+        Assert.Equal(2, result.Length);
+    }
+}
---
+++ TypingFast/Services/WordsService.cs
@@ -0,0 +1,27 @@
+using System.Reflection;
+
+namespace TypingFast.Services;
+
+public class WordsService
+{
+    private readonly string[] _words;
+
+    public WordsService()
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        var resourceName = "TypingFast.words.txt";
+
+        using (var stream = assembly.GetManifestResourceStream(resourceName))
+        using (var reader = new StreamReader(stream))
+        {
+            var content = reader.ReadToEnd();
+            _words = content.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+
+    public string[] GetWords(int count)
+    {
+        var random = new Random();
+        return _words.OrderBy(x => random.Next()).Take(count).ToArray();
+    }
+}
