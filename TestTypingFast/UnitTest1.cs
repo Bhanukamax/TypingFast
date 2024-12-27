8e29fbc60fd09c3e0b6bb48904cacd8669613de3
Bhanuka <bhanukamax@gmail.com>
2024-12-28 00:18:21 +0530

    Adding unit tests

--- TestTypingFast/UnitTest1.cs
+++ TestTypingFast/UnitTest1.cs
@@ -1,14 +1,27 @@
 ﻿using TypingFast.Services;
+using Xunit.Abstractions;
 
 namespace TestTypingFast;
 
 public class UnitTest1
 {
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public UnitTest1(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
     [Fact]
     public void Test1()
     {
         var wordsService = new WordsService();
-        var result = wordsService.GetWords(2);
-        Assert.Equal(2, result.Length);
+        var result = wordsService.GetWords(10);
+        foreach (var word in result)
+        {
+            _testOutputHelper.WriteLine(word);
+        }
+
+        Assert.Equal(10, result.Length);
     }
 }
